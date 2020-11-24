25f9fcd740bba6878f7907873a6df072d948b6ca
imbhav <bhavanivunnava@gmail.com>
2020-11-24 22:04:10 +0530

    adding server file

---
+++ form_validation/edit_data.php
@@ -0,0 +1,32 @@
+<?php
+
+
+if(isset($_POST['submit'])){
+    inserData();
+}
+
+
+function inserData(){
+    $edit_id = $_GET['id'];
+    include_once('db_config.php');
+    $name = $_POST['uname'];
+    $mobile = $_POST['umobile'];
+    $email = $_POST['uemail'];
+
+    $ins_query = mysqli_query($con, "UPDATE users
+    SET name = '$name', email= '$email', mobile= '$mobile'
+    WHERE id = $edit_id");
+
+    if($ins_query){
+        header("location: fetch.php");
+    }
+}
+
+
+
+
+
+
+?> 
+ 
+            
